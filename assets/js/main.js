735042b81d1dfcc67e7dc56c1b94118de994e0f9
Shafiq Hammad <shafiqhammad5@gmail.com>
2021-09-02 19:27:25 +0600

    submenu added

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,46 +32,6 @@
     }
   };
 
-  // ==== for menu scroll
-  // const pageLink = document.querySelectorAll(".ud-menu-scroll");
-
-  // pageLink.forEach((elem) => {
-  //   elem.addEventListener("click", (e) => {
-  //     e.preventDefault();
-  //     document.querySelector(elem.getAttribute("href")).scrollIntoView({
-  //       behavior: "smooth",
-  //       offsetTop: 1 - 60,
-  //     });
-  //   });
-  // });
-
-  // section menu active
-  // function onScroll(event) {
-  //   const sections = document.querySelectorAll(".ud-menu-scroll");
-  //   const scrollPos =
-  //     window.pageYOffset ||
-  //     document.documentElement.scrollTop ||
-  //     document.body.scrollTop;
-
-  //   for (let i = 0; i < sections.length; i++) {
-  //     const currLink = sections[i];
-  //     const val = currLink.getAttribute("href");
-  //     const refElement = document.querySelector(val);
-  //     const scrollTopMinus = scrollPos + 73;
-  //     if (
-  //       refElement.offsetTop <= scrollTopMinus &&
-  //       refElement.offsetTop + refElement.offsetHeight > scrollTopMinus
-  //     ) {
-  //       document.querySelector(".ud-menu-scroll").classList.remove("active");
-  //       currLink.classList.add("active");
-  //     } else {
-  //       currLink.classList.remove("active");
-  //     }
-  //   }
-  // }
-
-  // window.document.addEventListener("scroll", onScroll);
-
   //===== close navbar-collapse when a  clicked
   let navbarToggler = document.querySelector(".navbar-toggler");
   const navbarCollapse = document.querySelector(".navbar-collapse");
@@ -84,6 +44,15 @@
   );
   navbarToggler.addEventListener("click", function () {
     navbarToggler.classList.toggle("active");
+    navbarCollapse.classList.toggle("show");
+  });
+
+  // ===== submenu
+  const submenuButton = document.querySelectorAll(".nav-item-has-children");
+  submenuButton.forEach((elem) => {
+    elem.querySelector("a").addEventListener("click", () => {
+      elem.querySelector(".ud-submenu").classList.toggle("show");
+    });
   });
 
   // ===== wow js
