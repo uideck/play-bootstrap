214c436a4deebe3996a61678bae0387194d8296a
Shafiq Hammad <shafiqhammad5@gmail.com>
2021-09-02 16:09:59 +0600

    inner pages added

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,20 @@
 
   // ======= Sticky
   window.onscroll = function () {
-    const header_navbar = document.querySelector(".ud-header");
-    const sticky = header_navbar.offsetTop;
+    const ud_header = document.querySelector(".ud-header");
+    const sticky = ud_header.offsetTop;
     const logo = document.querySelector(".navbar-brand img");
 
     if (window.pageYOffset > sticky) {
-      header_navbar.classList.add("sticky");
+      ud_header.classList.add("sticky");
+    } else {
+      ud_header.classList.remove("sticky");
+    }
+
+    // === logo change
+    if (ud_header.classList.contains("sticky")) {
       logo.src = "assets/images/logo/logo-2.svg";
     } else {
-      header_navbar.classList.remove("sticky");
       logo.src = "assets/images/logo/logo.svg";
     }
 
@@ -28,50 +33,50 @@
   };
 
   // ==== for menu scroll
-  const pageLink = document.querySelectorAll(".page-scroll");
+  // const pageLink = document.querySelectorAll(".ud-menu-scroll");
 
-  pageLink.forEach((elem) => {
-    elem.addEventListener("click", (e) => {
-      e.preventDefault();
-      document.querySelector(elem.getAttribute("href")).scrollIntoView({
-        behavior: "smooth",
-        offsetTop: 1 - 60,
-      });
-    });
-  });
+  // pageLink.forEach((elem) => {
+  //   elem.addEventListener("click", (e) => {
+  //     e.preventDefault();
+  //     document.querySelector(elem.getAttribute("href")).scrollIntoView({
+  //       behavior: "smooth",
+  //       offsetTop: 1 - 60,
+  //     });
+  //   });
+  // });
 
   // section menu active
-  function onScroll(event) {
-    const sections = document.querySelectorAll(".page-scroll");
-    const scrollPos =
-      window.pageYOffset ||
-      document.documentElement.scrollTop ||
-      document.body.scrollTop;
+  // function onScroll(event) {
+  //   const sections = document.querySelectorAll(".ud-menu-scroll");
+  //   const scrollPos =
+  //     window.pageYOffset ||
+  //     document.documentElement.scrollTop ||
+  //     document.body.scrollTop;
 
-    for (let i = 0; i < sections.length; i++) {
-      const currLink = sections[i];
-      const val = currLink.getAttribute("href");
-      const refElement = document.querySelector(val);
-      const scrollTopMinus = scrollPos + 73;
-      if (
-        refElement.offsetTop <= scrollTopMinus &&
-        refElement.offsetTop + refElement.offsetHeight > scrollTopMinus
-      ) {
-        document.querySelector(".page-scroll").classList.remove("active");
-        currLink.classList.add("active");
-      } else {
-        currLink.classList.remove("active");
-      }
-    }
-  }
+  //   for (let i = 0; i < sections.length; i++) {
+  //     const currLink = sections[i];
+  //     const val = currLink.getAttribute("href");
+  //     const refElement = document.querySelector(val);
+  //     const scrollTopMinus = scrollPos + 73;
+  //     if (
+  //       refElement.offsetTop <= scrollTopMinus &&
+  //       refElement.offsetTop + refElement.offsetHeight > scrollTopMinus
+  //     ) {
+  //       document.querySelector(".ud-menu-scroll").classList.remove("active");
+  //       currLink.classList.add("active");
+  //     } else {
+  //       currLink.classList.remove("active");
+  //     }
+  //   }
+  // }
 
-  window.document.addEventListener("scroll", onScroll);
+  // window.document.addEventListener("scroll", onScroll);
 
   //===== close navbar-collapse when a  clicked
   let navbarToggler = document.querySelector(".navbar-toggler");
   const navbarCollapse = document.querySelector(".navbar-collapse");
 
-  document.querySelectorAll(".page-scroll").forEach((e) =>
+  document.querySelectorAll(".ud-menu-scroll").forEach((e) =>
     e.addEventListener("click", () => {
       navbarToggler.classList.remove("active");
       navbarCollapse.classList.remove("show");
