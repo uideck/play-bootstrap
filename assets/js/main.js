5102685ef485554091d8e35483139c2a11d2df2e
Vitor Mattos <vitor@php.rio>
2023-12-04 17:48:46 -0300

    First commit of LibreSign content
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,9 +15,9 @@
 
     // === logo change
     if (ud_header.classList.contains("sticky")) {
-      logo.src = "assets/images/logo/logo-2.svg";
+      logo.src = "assets/images/logo/logo-2.png";
     } else {
-      logo.src = "assets/images/logo/logo.svg";
+      logo.src = "assets/images/logo/logo.png";
     }
 
     // show or hide the back-top-top button
